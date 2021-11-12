40d089052437730187908e85b9148641d87f2298
Xiong Shihao <shihao.xiong@icloud.com>
2021-11-11 23:13:11 -0500

    add posts create page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const home = require('./home');
 const authorize = require('./authorize');
+const posts = require('./posts');
 
 module.exports = app => {
 	app.use('/', home);
 	app.use('/authorize', authorize);
+	app.use('/posts', posts);
 
 	app.use('*', (_, res) => {
 		res.status(404).json({ error: 'No APIs' });
