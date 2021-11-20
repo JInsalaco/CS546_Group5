9229d6786721fb5fd5f24cd195481174263a0305
Xiong Shihao <shihao.xiong@icloud.com>
2021-11-19 19:33:18 -0500

    dev: profile/upload

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const { formidable } = require('formidable');
+const fs = require('fs');
 
-router.get('/:part', (req, res) => {
+router.get('/:part', (_, res) => {
 	res.render('profile', { title: 'Profile' });
+});
+
+router.post('/upload', (req, res) => {
+	const form = new formidable.IncomingForm();
+
+	form.parse(req, (err, fields, files) => {
+		if (err) return;
+
+		try {
+			const { newFilename, filepath, originalFilename } = files.avatar;
+			const type = originalFilename.match(/\.\w+$/)[0];
+			const fileName = `${newFilename}${type}`;
+			fs.writeFileSync(`public/img/${fileName}`, fs.readFileSync(filepath));
+			res.json({ path: `/public/img/${fileName}` });
+		} catch (error) {
+			res.status(500).send('Interval Error.');
+		}
+	});
 });
 
 module.exports = router;
