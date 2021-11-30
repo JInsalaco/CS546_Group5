82b9e00a2baeb51d493cea06b17eb982742ba693
Joseph Insalaco <joseph.insalaco1@gmail.com>
2021-11-30 11:26:36 -0500

    Added functions to comments and communities

---
+++ data/comments.js
@@ -0,0 +1,49 @@
+const { comments } = require('../config/mongoCollections');
+const utils = require('./utils');
+
+async function createComment(body,posterId,thread){
+    const commentCollection = await comments();
+    const d = new Date();
+    const newComment = {
+        body: body,
+        posterId: posterId,
+        thread: thread,
+        popularity: {},
+        metaData: {
+            timeStamp: d,
+            archived: false,
+            flags: 0
+        }
+    }
+    const insertInfo = await commentCollection.insertOne(newComment);
+    if (insertInfo.insertedCount === 0) throw "Error: Could not add topic";
+
+    return insertInfo;
+}
+async function commentPopularity(commentId,userId,val){
+    const cid = utils.stringToObjectID(commentId);
+    const commentCollection = await comments();
+    const comment = commentCollection.findOne({_id: cid})
+    let popularityObj = comment.popularity;
+    popularityObj[userId] = val;
+    const updateInfo = commentCollection.updateOne({_id:cid},
+        {$set: 
+            {popularity: popularityObj}
+        });
+    if(updateInfo.modifiedCount === 0) throw "Error: could not update popularity";
+    return updateInfo;
+}
+
+async function deleteComment(commentId){
+    const cid = utils.stringToObjectID(commentId);
+    const commentCollection = await comments();
+    const deletionInfo = commentCollection.deleteOne({_id: cid});
+    if (deletionInfo.deletedCount === 0) { throw `Could not delete topic`; }
+    return { deleted: true };
+}
+
+module.exports = {
+    createComment,
+    commentPopularity,
+    deleteComment
+}
