ec648a22e50c1b62b1a9ca1b8c48862c446d5383
Riya Kharbanda <kharbanda.riya.97@gmail.com>
2021-12-03 15:40:50 -0500

    created route `/posts` GET

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const postData = require('../data/posts');
 const topicData = require('../data/topics');
+const userData = require('../data/users')
 
 router.get('/:id', async (req, res) => {
 	const idCheck = errorCheckingId(req.params.id);
@@ -20,6 +21,21 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
+router.get('/', async (req, res) => {
+	
+	try {
+		if (!req.session.userid) {
+			// return res.redirect('/partials/authorize/SignInForm');
+			res.status(403).send('Please Sign in');
+		} else {
+			const { topicId, pageSize, pageNumber, title } = req.query;
+			const post = await postData.getAllPosts();
+			res.status(200).json({post});
+		}
+	} catch (e) {
+		return res.status(400).send(e);
+	}
+});
 router.post('/add', async (req, res) => {
 	try {
 		if (!req.session.userid) {
@@ -28,9 +44,9 @@ router.post('/add', async (req, res) => {
 
 		const { title, body, topics } = req.body;
 		postData.errorCheckingPost(title, body);
-		const topicResult = await topicData.getAllTopicTitles(topics);
+		// const topicResult = await topicData.getAllTopicTitles(topics);
 		const posterId = req.session.userid;
-		const result = await postData.addPost(posterId, title, body, topicResult);
+		const result = await postData.addPost(posterId, title, body, topics);
 		if (result) res.status(200).send('Posted Successfully, check your feed!');
 		else res.status(500).send('Internal server error, please try again after some time');
 	} catch (e) {
